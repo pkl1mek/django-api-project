6893eab6129e3c2d7feaa08aebf388bc54d7e381
patrykklim3k <patryk@discurso.ai>
2025-12-17 13:36:58 +0100

    Backend setup

---
+++ backend/djangoapi/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Project, ProjectImage
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ['id', 'image']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    images = ProjectImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'images', 'created_at']
